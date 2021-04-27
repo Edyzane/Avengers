224b4d0c3e7641c8c5a65ee4c48a0d0f64939d4b
mell <melledzan@gmail.com>
2021-04-27 10:29:58 +0800

    added updated running flask(design nt done)

--- Mell/housepred.py
+++ Mell/housepred.py
@@ -13,24 +13,28 @@ def return_prediction(model,scaler,sample_json):
     # For larger data features, you should probably write a for loop
     # That builds out this array for you
 
-    LotArea = sample_json['LotArea']
-    YearBuilt = sample_json['YearBuilt']
-    YearRemodAdd = sample_json['YearRemodAdd']
-    MasVnrArea = sample_json['MasVnrArea']
-    BsmtFinSF1 = sample_json['BsmtFinSF1']
-    TotalBsmtSF = sample_json['TotalBsmtSF']
-    dndFlrSF = sample_json['dndFlrSF']
-    GrLivArea = sample_json['GrLivArea']    
-    GarageArea = sample_json['GarageArea']
-    WoodDeckSF = sample_json['WoodDeckSF']
-    OpenPorch = sample_json['OpenPorchSF']
-    MiscVal = sample_json['MiscVal']
+    L_Area = sample_json['Lot Area']
+    Y_Built = sample_json['Year Built']
+    Y_RemodAdd = sample_json['Year RemodAdd']
+    M_Vnr_Area = sample_json['Mas Vnr Area']
+    B_SF_1 = sample_json['BsmtFin SF 1']
+    T_Bsmt_SF = sample_json['Total Bsmt SF']
+    S_Flr_SF = sample_json['Second Flr SF']
+    G_Liv_Area = sample_json['Gr Liv Area']
+    G_Area = sample_json['Garage Area']
+    W_Deck_SF = sample_json['Wood Deck SF']
+    O_Porch_SF = sample_json['Open Porch SF']
+    M_Val = sample_json['petal width']
 
 
-    house = [[LotArea, YearBuilt, YearRemodAdd, MasVnrArea,BsmtFinSF1,TotalBsmtSF,dndFlrSF,GrLivArea,GarageArea,WoodDeckSF,OpenPorch,MiscVal]]
-    house = scaler.transform(house)
-    prediction = model.predict(house)
-    
+  
+
+    flower = [[L_Area,Y_Built, Y_RemodAdd, M_Vnr_Area,B_SF_1,T_Bsmt_SF,S_Flr_SF,G_Liv_Area,G_Area,W_Deck_SF,O_Porch_SF,M_Val]]
+
+    flower = scaler.transform(flower)
+
+    prediction = model.predict(flower)
+
     return prediction[0]
 
 
@@ -50,20 +54,19 @@ house_scaler = load("Ames_Housing_scaler(v2).pkl")
 # Now create a WTForm Class
 # Lots of fields available:
 # http://wtforms.readthedocs.io/en/stable/fields.html
-class HouseForm(FlaskForm):
+class houseForm(FlaskForm):
     Lot_Area = TextField('Lot Area')
     Year_Built = TextField('Year Built')
     Year_RemodAdd = TextField('Year RemodAdd')
     Mas_Vnr_Area = TextField('Mas Vnr Area')
     BsmtFin_SF_1 = TextField('BsmtFin SF 1')
     Total_Bsmt_SF = TextField('Total Bsmt SF')
-    dnd_Flr_SF = TextField('dnd Flr SF')
+    Second_flr_SF = TextField('Second Flr SF')
     Gr_Liv_Area = TextField('Gr Liv Area')
     Garage_Area = TextField('Garage Area')
     Wood_Deck_SF = TextField('Wood Deck SF')
     Open_Porch_SF = TextField('Open Porch SF')
     Misc_Val = TextField('Misc Val')
-
 
     submit = SubmitField('Analyze')
 
@@ -73,7 +76,7 @@ class HouseForm(FlaskForm):
 def index():
 
     # Create instance of the form.
-    form = HouseForm()
+    form = houseForm()
     # If the form is valid on submission (we'll talk about validation next)
     if form.validate_on_submit():
         # Grab the data from the breed on the form.
@@ -84,7 +87,7 @@ def index():
         session['Mas_Vnr_Area'] = form.Mas_Vnr_Area.data
         session['BsmtFin_SF_1'] = form.BsmtFin_SF_1.data
         session['Total_Bsmt_SF'] = form.Total_Bsmt_SF.data
-        session['dnd_Flr_SF'] = form.dnd_Flr_SF.data
+        session['Second_flr_SF'] = form.Second_flr_SF.data
         session['Gr_Liv_Area'] = form.Gr_Liv_Area.data
         session['Garage_Area'] = form.Garage_Area.data
         session['Wood_Deck_SF'] = form.Wood_Deck_SF.data
@@ -102,18 +105,18 @@ def prediction():
 
     content = {}
 
-    content['LotArea'] = float(session['Lot_Area'])
-    content['YearBuilt'] = float(session['Year_Built'])
-    content['YearRemodAdd'] = float(session['Year_RemodAdd'])
-    content['MasVnrArea'] = float(session['Mas_Vnr_Area'])
-    content['BsmtFinSF1'] = float(session['BsmtFin_SF_1'])
-    content['TotalBsmtSF'] = float(session['Total_Bsmt_SF'])
-    content['dndFlrSF'] = float(session['dnd_Flr_SF'])
-    content['GrLivArea'] = float(session['Gr_Liv_Area'])
-    content['GarageArea'] = float(session['Garage_Area'])
-    content['WoodDeckSF'] = float(session['Wood_Deck_SF'])
-    content['OpenPorchSF'] = float(session['Open_Porch_SF'])
-    content['MiscVal'] = float(session['Misc_Val'])
+    content['Lot Area'] = float(session['Lot_Area'])
+    content['Year Built'] = float(session['Year_Built'])
+    content['Year RemodAdd'] = float(session['Year_RemodAdd'])
+    content['Mas Vnr Area'] = float(session['Mas_Vnr_Area'])
+    content['BsmtFin SF 1'] = float(session['BsmtFin_SF_1'])
+    content['Total Bsmt SF'] = float(session['Total_Bsmt_SF'])
+    content['Second Flr SF'] = float(session['Second_flr_SF'])
+    content['Gr Liv Area'] = float(session['Gr_Liv_Area'])
+    content['Garage Area'] = float(session['Garage_Area'])
+    content['Wood Deck SF'] = float(session['Wood_Deck_SF'])
+    content['Open Porch SF'] = float(session['Open_Porch_SF'])
+    content['petal width'] = float(session['Misc_Val'])
 
     results = return_prediction(model=house_model,scaler=house_scaler,sample_json=content)
 
